84a319ebb7e6beddb577a036690595edb35c505b
Ibrahim Abdelaal <shaker.personnel@gmail.com>
2023-02-22 17:48:40 +0200

    insert index.html inside src folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
